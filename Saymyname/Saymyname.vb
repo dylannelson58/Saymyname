421a7e6ab3deaedc005bd7dcf7fe1b8c368026f0
Dylan Nelson <dylannelson58@gmail.com>
2021-01-24 09:37:33 -0700

    add name input and output

---
+++ Saymyname/Saymyname.vb
@@ -0,0 +1,25 @@
+﻿Option Explicit On
+Option Strict On
+
+'Dylan Nelson
+'RCET0265
+'Spring 2021
+'Say My Name
+'https://github.com/
+
+Module Saymyname
+
+    Sub Main()
+
+        Dim userInput As String
+
+        Console.WriteLine("Hello, what is your name?")
+
+        userInput = Console.ReadLine
+
+        Console.WriteLine("Hi, " & userInput & ". Nice to meet you!")
+
+        Console.Read()
+    End Sub
+
+End Module
